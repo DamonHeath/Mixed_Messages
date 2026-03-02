8e906048d5457b1dc0567dc82c94704512ebd738
unknown <damon.james.heath@gmail.com>
2026-03-02 08:29:58 +0000

    index.js updated with first basic array and the main randomIndex variable with Math.floor(Math.floor(Math.random() * mission.length)

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const mission = [
+    "While crossing the Crimson Nebula, your ship alarms start screaming.",
+    "You land on a frozen moon and discover your fuel cells are missing.",
+    "A space station hails you with a message that is definitely a trap."
+];
+const randomIndex = (Math.floor(Math.random() * mission.length))
+
 console.log("🚀 Mission initialising...");
+console.log(mission[randomIndex])
